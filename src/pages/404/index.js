a7a2b19eb5d77fb322fa28c34bbf653b93307ac7
kalfritz <luizeliasceem@gmail.com>
2020-07-16 16:25:53 -0300

    add 404 page

---
+++ src/pages/404/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import { Container } from "./styles"
+
+function FourZeroFour() {
+  return (
+    <Container>
+      <h1>Sorry. This page does not exist</h1>
+    </Container>
+  )
+}
+export default FourZeroFour
