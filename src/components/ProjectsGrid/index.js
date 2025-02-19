506d3df2367638571c8235b7e4bc64841345f814
Luiz Ramos <luizeliasceem@gmail.com>
2025-02-19 19:42:22 -0300

    move files to static

--- src/components/ProjectsGrid/index.js
+++ src/components/ProjectsGrid/index.js
@@ -3,17 +3,11 @@ import metadata from "../../../config/siteMetaData"
 import Project from "../Project"
 import { Container } from "./styles"
 
-import maestro from "../../assets/maestro.png"
-import maestroGif from "../../assets/maestro.gif"
-import socihub from "../../assets/socihub.jpg"
-import socihubGif from "../../assets/socihub.gif"
-import pokestore from "../../assets/pokestore.png"
-import pokeGif from "../../assets/pokestore.gif"
-
-const map = {}
-map["Maestro"] = { img: maestro, gif: maestroGif }
-map["SociHub"] = { img: socihub, gif: socihubGif }
-map["PokeStore"] = { img: pokestore, gif: pokeGif }
+const map = {
+  "Maestro": { img: "/maestro.png", gif: "/maestro.gif" },
+  "SociHub": { img: "/socihub.jpg", gif: "/socihub.gif" },
+  "PokeStore": { img: "/pokestore.png", gif: "/pokestore.gif" }
+};
 
 function ProjectsGrid() {
   return (
