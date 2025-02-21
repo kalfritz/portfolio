75ea8d1cce4f267f8fa7be15efa2a55d80934e61
Luiz Ramos <luizeliasceem@gmail.com>
2025-02-21 11:02:02 -0300

    remove analytics

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Analytics } from "@vercel/analytics"
 import { Helmet } from 'react-helmet'
 
 const Layout = ({ children }) => (
@@ -12,7 +11,6 @@ const Layout = ({ children }) => (
       />
     </Helmet>
     <main>{children}</main>
-    <Analytics />
   </>
 )
 
